40c984385820c4899f68b6f9d8eb3c30e503576f
Abraham Fong <abefong54@gmail.com>
2019-01-28 12:04:34 -0600

    Delete alexa.py
    useless

--- alexa.py
+++
@@ -1,8 +0,0 @@
-import smartcar
-from flask import Flask, redirect, request, jsonify
-from flask_cors import CORS
-
-def testing(carName, mileage):
-    print("this works you son of a bitch!!")
-
-    print(carName, mileage)
